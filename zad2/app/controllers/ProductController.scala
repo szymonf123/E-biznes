a809cc98df442fc205b1cc4db37d6f87f08ec830
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2025-03-26 14:33:27 +0100

    Added CRUD endpoints (3.5)

--- zad2/app/controllers/ProductController.scala
+++ zad2/app/controllers/ProductController.scala
@@ -1,9 +1,7 @@
 package controllers
 import javax.inject._
-import play.api._
 import play.api.mvc._
 import play.api.libs.json._
-
 import models.Product
 
 object Product {
@@ -19,7 +17,44 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
     new Product("Tablet", 1200.00)
   )
 
-  def index: Action[AnyContent] = Action {
-    Ok(views.html.index(products))
+  def showAll: Action[AnyContent] = Action {
+    Ok(views.html.index("Lista produktów", products))
   }
+
+  def showById(id: Int): Action[AnyContent] = Action {
+    products.lift(id) match {
+      case Some(product) =>
+        Ok(views.html.index(s"Produkt o ID $id", List(product)))
+      case None =>
+        NotFound(s"Produkt o ID $id nie istnieje.")
+    }
+  }
+
+  def add(name: String, price: Double): Action[AnyContent] = Action {
+    products = products :+ new Product(name, price)
+    Ok(views.html.index(s"Dodano nowy produkt - $name", products))
+  }
+
+  def delete(id: Int): Action[AnyContent] = Action {
+    products.lift(id) match {
+      case None =>
+        NotFound(s"Produkt o ID $id nie istnieje.")
+      case Some(product) =>
+        val droppedProductName = product.name
+        products = products.patch(id, Nil, 1)
+        Ok(views.html.index(s"Usunięto produkt - $droppedProductName", products))
+    }
+  }
+
+  def update(id: Int, name: String, price: Double): Action[AnyContent] = Action {
+    products.lift(id) match {
+      case None =>
+        NotFound(s"Produkt o ID $id nie istnieje.")
+      case Some(product) =>
+        val updatedProduct = product.copy(name = name, price = price)
+        products = products.updated(id, updatedProduct)
+        Ok(views.html.index(s"Zmodyfikowano produkt o ID $id", products))
+    }
+  }
+
 }
