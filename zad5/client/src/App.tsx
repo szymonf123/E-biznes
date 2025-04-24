4a9a71437adb7511f477f0296677a6e58989ce35
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2025-04-24 20:11:49 +0200

    Add payments

---
+++ zad5/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Payment from './components/Payment';
+
+const App: React.FC = () => {
+  return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <Payment />
+      </div>
+  );
+};
+
+export default App;
