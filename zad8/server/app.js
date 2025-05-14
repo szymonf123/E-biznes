d2172882c09f799e1924c23e2888c6394072a02f
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2025-05-15 01:07:48 +0200

    Add logging and creating an account without OAuth2 (3.5)

---
+++ zad8/server/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./config/database");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Serwer działa na http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("Serwer uruchomiony");
+})
+
+module.exports = app;
