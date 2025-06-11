d48e43c434862b753a9a496565413dcf78a16c54
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2025-06-11 22:31:46 +0200

    Add deployment by GH actions (4.5)

--- zad5/server/main.go
+++ zad5/server/main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"*", "http://localhost:3000", "https://zad5-frontend-app.azurewebsites.net", "http://zad5-frontend-app.azurewebsites.net"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://zad5-frontend-app.azurewebsites.net", "http://zad5-frontend-app.azurewebsites.net"},
 		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodOptions},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowCredentials: true,
